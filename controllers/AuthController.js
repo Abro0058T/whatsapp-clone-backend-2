a189aa43e6f8813a2db69538980badf6a7c1a116
AbhiAtiUttam <Abhi@glocalize.net>
2024-08-18 14:24:24 +0530

    added voideo call and voice call feature

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,5 +1,5 @@
 import getPrismaInstance from "../utils/PrismaClient.js"
-
+import {generateToken04} from "../utils/TokenGenerator.js"
 export const checkUser=async (req,res,next)=>{
     try{
         const {email}=req.body
@@ -67,6 +67,24 @@ export const getAllusers = async (req, res, next) =>{
         })
         return res.status(200).send({users:usersGroupYInitialLetter})
     }catch(error){
-        next(errpr);
+        next(error);
+    }
+}
+
+
+export const generateToken = (req, res, next) => {
+    try{
+        const appId = parseInt(process.env.ZEGO_APP_ID)
+        const serverSecret = process.env.ZEGO_SERVER_ID
+        const userId = req.params.userId;
+        const  effectiveTime = 3600;
+        const payload = ""
+        if(appId && serverSecret && userId) {
+            const token = generateToken04(appId, userId, serverSecret, effectiveTime, payload );
+            res.status(200).json({token});
+        }
+        return res.status(400).send("User id, app id and server secret is reqired.")
+    }catch(err) {
+        next(err)
     }
 }
--- routes/AuthRoutes.js
+++ routes/AuthRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { checkUser, getAllusers, onBoardUser } from "../controllers/AuthController.js";
+import { checkUser, generateToken, getAllusers, onBoardUser } from "../controllers/AuthController.js";
 
 const router= Router()
 
 router.post("/check-user",checkUser);
 router.post("/onboard-user",onBoardUser)
 router.get("/get-contacts",getAllusers)
+router.get("/generate-token/:userId",generateToken)
 
 export default router
